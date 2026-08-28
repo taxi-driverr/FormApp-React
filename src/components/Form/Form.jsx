88423765a186fbd7f1f48e5bf34dde9450abdc1a
taxi_driverr <xerxesxerxes13@gmail.com>
2026-08-28 09:15:26 +0530

    added ref

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import validatePassword from "../../helper/passwordValidator.js";
 import validateEmail from "../../helper/emailValidator.js"
 import "./Form.css";
 function Form() {
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
 
     const[formValues, setFormValues] = useState({
         email: "",
@@ -22,23 +25,29 @@ function Form() {
     const handleValidatePassword = () => {
         const password = formValues.password;
         if(!validatePassword(password)){
-            console.log("password does not contain required params");
+            emailRef.current.focus();
+            const emailVal = emailRef.current.value;
+            console.log("password does not contain required params", emailVal);
         }
     }
 
     const handleValidateEmail = () => {
         const email = formValues.email;
-        if(!validateEmail(email)) 
-            console.log("not a valid email")
+        if(!validateEmail(email)) {
+            passwordRef.current.focus();
+            const passwordVal = passwordRef.current.value;
+            console.log("not a valid email", passwordVal);
+        }
     }
     return(
         <>
             <form onSubmit={handleFormSubmit}>
                 <div className="wrapper email-input-wrapper">
                     <input 
-                    type="email" 
+                    type="text" 
                     placeholder="Enter your email address" 
                     value={formValues.email}
+                    ref={emailRef}
                     onChange={(e) => setFormValues({...formValues, email: e.target.value})}
                     />
                 </div>
@@ -47,6 +56,7 @@ function Form() {
                     type="password" 
                     placeholder="Enter your password" 
                     value={formValues.password}
+                    ref={passwordRef}
                     onChange={(e) => setFormValues({...formValues, password: e.target.value})}
                     />
                 </div>
